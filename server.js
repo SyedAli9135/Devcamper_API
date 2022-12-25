72d91a558f27996a19fba24c2686324f0e79501b
syedali9135 <syedaliakhtar660@gmail.com>
2022-12-25 22:12:20 +0500

    bootcamps middleware crud courses middleware crud routers of both

--- server.js
+++ server.js
@@ -1,14 +1,56 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const fileupload = require("express-fileupload");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to Database
+connectDB();
+
+//Route files
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+
 const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// File Uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
+
+// Handle Unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close Server and Exit
+  server.close(() => process.exit(1));
+});
